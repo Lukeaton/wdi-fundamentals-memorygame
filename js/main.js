0c4c971801905f75434211f5c12e876bff04a04e
Luke Eaton <lukeaton@gmail.com>
2018-06-11 16:15:36 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 console.log("User flipped " + cardOne);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = ['0'];
+cardsInPlay.push('cardOne');
+console.log("User flipped queen");
+var cardTwo = ['2'];
+cardsInPlay.push('cardTwo');
+console.log('User Flipped king');
+if (cardsInPlay.length === 2) { 
+	console.log("you have selected two cards"); 
+	if (cardsInPlay[0] === cardsInPlay[1]) 
+		alert("You found a match!"); 
+else alert("Sorry, try again"); } 
